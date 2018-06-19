bde1c599065bd84d5f03b2085557c460dec8a18a
Abhisek <abhisekbunty94@gmail.com>
2018-06-19 10:06:48 +0530

    Removed  redundant code.

--- MVVM-3/RefactoredNearby/Nearby/Nearby/Extensions/UICollectionView+Additions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UICollectionView+Additions.swift
-//  Nearby
-//
-//  Created by Abhisek on 5/24/18.
-//  Copyright © 2018 Abhisek. All rights reserved.
-//
-
-import Foundation
--- MVVM-3/RefactoredNearby/Nearby/Pods/Target Support Files/Pods-NearbyUITests/Pods-NearbyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NearbyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NearbyUITests
-@end
